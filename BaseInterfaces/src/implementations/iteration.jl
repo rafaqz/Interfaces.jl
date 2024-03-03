54210a455f529276f90caf50b86d7d1e8f26a5c9
rafaqz <rafaelschouten@gmail.com>
2024-03-03 11:42:58 +0100

    delete IterationInteferface implementations, theyre inherited now

--- BaseInterfaces/src/implementations/iteration.jl
+++ BaseInterfaces/src/implementations/iteration.jl
@@ -1,8 +1,3 @@
-
-# @implements IterationInterface{(:reverse,:indexing)} UnitRange [1:5, -2:2]
-# @implements IterationInterface{(:reverse,:indexing)} StepRange [1:2:10, 20:-10:-20]
-# @implements IterationInterface{(:reverse,:indexing)} Array [[1, 2, 3, 4], [:a :b; :c :d]]
-# @implements IterationInterface{(:reverse,:indexing)} Tuple [(1, 2, 3, 4)]
 @static if VERSION >= v"1.9.0"
     @implements IterationInterface{(:reverse,:indexing)} NamedTuple [(a=1, b=2, c=3, d=4)]
 else
@@ -13,12 +8,3 @@ end
 @implements IterationInterface Number [1, 1.0, 1.0f0, UInt(8), false]
 @implements IterationInterface{:reverse} Base.Generator [(i for i in 1:5), (i for i in 1:5)]
 # @implements IterationInterface{(:reverse,:indexing)} Base.EachLine [eachline(joinpath(dirname(pathof(BaseInterfaces)), "implementations.jl"))]
-
-# @implements IterationInterface Set [Set((1, 2, 3, 4))]
-# @implements IterationInterface BitSet [BitSet((1, 2, 3, 4))]
-# @implements IterationInterface Dict [Dict("a" => 2, :b => 3.0)]
-# @implements IterationInterface Base.EnvDict [Arguments(d=Base.EnvDict())]
-# @implements IterationInterface Base.ImmutableDict [Arguments(d=Base.ImmutableDict(:a => 1, :b => 2))]
-# @implements IterationInterface IdDict
-# @implements IterationInterface WeakKeyDict
-
