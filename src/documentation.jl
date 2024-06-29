a8f20e70e96f44c9fe96129630c0e86b0888e854
MilesCranmer <miles.cranmer@gmail.com>
2024-06-29 19:37:29 +0100

    account for multiple methods in extended help

--- src/documentation.jl
+++ src/documentation.jl
@@ -4,22 +4,37 @@ function header(interface::Type{<:Interface})
     return "An Interfaces.jl `Interface` with mandatory components `$m_keys` and optional components `$o_keys`."
 end
 
-function extended_help(@nospecialize(interface::Type{<:Interface}))
-    m_keys = mandatory_keys(interface)
-    o_keys = optional_keys(interface)
+function extended_help(interface::Type{<:Interface})
+    comp = components(interface)
 
     io_buf = IOBuffer()
     # ^(More efficient and readable than string concatenation)
 
-    println(io_buf, "# Extended help\n")
-    println(io_buf, "## Mandatory keys:\n")
-    for (key, (value, _)) in m_keys
-        println(io_buf, "* `$key`: $value")
+    println(io_buf, "# Extended help")
+    !isempty(comp.mandatory) && println(io_buf, "\n## Mandatory keys:\n")
+    for key in keys(comp.mandatory)
+        values = comp.mandatory[key]
+        if values isa Tuple
+            println(io_buf, "* `$key`:")
+            for value in values
+                println(io_buf, "  * $(first(value))")
+            end
+        else
+            println(io_buf, "* `$key`: $(first(values))")
+        end
     end
 
-    println(io_buf, "\n## Optional keys:\n")
-    for (key, (value, _)) in o_keys
-        println(io_buf, "* `$key`: $value")
+    !isempty(comp.optional) && println(io_buf, "\n## Optional keys:\n")
+    for key in keys(comp.optional)
+        values = comp.optional[key]
+        if values isa Tuple
+            println(io_buf, "* `$key`:")
+            for value in values
+                println(io_buf, "  * $(first(value))")
+            end
+        else
+            println(io_buf, "* `$key`: $(first(values))")
+        end
     end
 
     return String(take!(io_buf))
