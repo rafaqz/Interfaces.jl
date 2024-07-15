f63e15f30e70ed59c10859020d0e8499f14473ce
rafaqz <rafaelschouten@gmail.com>
2024-07-15 20:54:50 +0200

    limit LazyString to > v1.8

--- BaseInterfaces/src/implementations/string.jl
+++ BaseInterfaces/src/implementations/string.jl
@@ -3,5 +3,7 @@
 
 @implements StringInterface{:length} String ["abc"]
 @implements StringInterface{:length} SubString [view("abc", 2:3)]
-@implements StringInterface{:length} LazyString [LazyString("abc", "def")]
 @implements StringInterface{:length} SubstitutionString [SubstitutionString("abc \\1")]
+if VERSION > v"1.8.0"
+    @implements StringInterface{:length} LazyString [LazyString("abc", "def")]
+end
